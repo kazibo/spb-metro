cb6102bb02db6b1c36bc4410913d9a14d566d859
Igor <i.atsberger@gmail.com>
2018-02-25 14:47:20 +0200

    First commit

---
+++ Metro/src/project1/StationButton.java
@@ -0,0 +1,14 @@
+package project1;
+
+import javafx.scene.paint.Paint;
+
+public class StationButton extends javafx.scene.shape.Circle {
+	
+	public int assignLine;
+	public int assignStation;
+	
+	public StationButton(double centerX, double centerY,  Paint fill) {
+		super(centerX, centerY, 7, fill);
+	}
+
+}
